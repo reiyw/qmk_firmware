671248b1c7b147ef2534352ed88ba71e58266e5e
reiyw <reiyw.setuve@gmail.com>
2019-11-29 11:19:16 +0900

    pgup pgdn

--- keyboards/lily58/keymaps/reiyw/keymap.c
+++ keyboards/lily58/keymaps/reiyw/keymap.c
@@ -94,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT( \
   _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______, \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-  _______,  _______,    _______,   KC_BTN2,   KC_BTN1,   _______,                       XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ESC,  \
+  _______,  KC_PGDN,    KC_PGUP,   KC_BTN2,   KC_BTN1,   _______,                       XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ESC,  \
   _______,   _______,   _______,   _______,  _______,  _______,   _______, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
                              _______, _______, _______,  _______, _______,  _______, _______, _______ \
 ),
